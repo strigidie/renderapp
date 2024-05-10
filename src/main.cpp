2b1ab4591a720ebef26242832cfc57936e2a27c0
George Vikulov <20969918+strigidie@users.noreply.github.com>
2024-05-10 03:01:56 +0200

    Initial structure

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// -----------------------------------------------------------------------------
+// Entry Point
+// -----------------------------------------------------------------------------
+int main(
+    const int argc,
+    const char* const* argv,
+    const char* const* envp)
+{
+
+    return EXIT_SUCCESS;
+}
