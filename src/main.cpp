9f446ffae201bebee69cc519eb8d2add7f7e4786
George Vikulov <20969918+strigidie@users.noreply.github.com>
2024-05-10 15:55:11 +0200

    Libraries setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+// #include "glew.h"
+
+#include "SDL.h"
+#include "SDL_opengl.h"
+
+// #include "imgui.h"
+// #include "imgui_impl_sdl2.h"
+// #include "imgui_impl_opengl3.h"
+
+#include "glm.hpp"
+
 // -----------------------------------------------------------------------------
 // Entry Point
 // -----------------------------------------------------------------------------
@@ -8,6 +19,13 @@ int main(
     const char* const* argv,
     const char* const* envp)
 {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow(
+        "RenderApp",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL
+    );
 
     return EXIT_SUCCESS;
 }
